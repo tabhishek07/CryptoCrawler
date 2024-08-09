b65b0bca5b01e8f4eb3566745c00ad1e313d412c
Abhishek Tiwari <163037775+tabhishek07@users.noreply.github.com>
2024-08-09 09:26:57 +0530

    worked on top 10 page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Card from "./Card";
 
-const Cards = ({ apiData }) => {
+const Cards = ({ apiData, checker }) => {
 
   console.log("api data in Cards",apiData);
   return (
     <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 px-5">
-      {apiData.map((element) => (
-        <Card item={element} key={element.id} />
+      {apiData.map((element, index) => (
+        <Card 
+         item={ checker === 'top10' ? element.item :element}
+          key={ checker === 'top10' ? element.item.id : element.id} 
+        checker = {checker}  
+        />
       ))}
     </div>
   );
--- src/pages/Top10.js
+++ src/pages/Top10.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cards from "../components/Cards";
 
 const Top10 = () => {
-  return <div>Top10</div>;
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url ="https://api.coingecko.com/api/v3/search/trending/";
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then(( data ) => {
+        setData(data);
+      })
+      .catch(( error) => {
+        console.log(error);
+      });
+      console.log(data)
+  }, []);
+  return data.length === 0 ?(
+    <div>UI Loading</div>):(
+      <div className="bg-gradient-to-r from-neutral-900 via-gray-800 to-rose-900 pb-4">
+      <Cards apiData={data.coins} checker={"top10"} />
+    </div>
+    )
+    
 };
 
 export default Top10;
